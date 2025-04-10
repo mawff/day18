3040eda749a846c6613755b4946ea6e5cf9a88df
mawff <arhipceva8@gmail.com>
2025-04-10 10:56:03 +0300

    добавлен класс Hotel

---
+++ day18/Hotel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day18
+{
+    internal class Hotel
+    {   public int id { get; set; }
+        public string Home { get; set; }
+        public int GameOfStars { get; set; }
+        public string CountryName { get; set; }
+        public string HotelImage { get; set; }
+    }
+}
