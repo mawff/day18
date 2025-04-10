b966e4d90720df27357276f30c0c77fada2f9df8
mawff <arhipceva8@gmail.com>
2025-04-10 11:10:08 +0300

    add pole in hotel

--- day18/Hotel.cs
+++ day18/Hotel.cs
@@ -14,5 +14,6 @@ namespace day18
         public int GameOfStars { get; set; }
         public string CountryName { get; set; }
         public string HotelImage { get; set; }
+        public string HotelIamgayyy { get; set; }
     }
 }
